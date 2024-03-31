ca9e892946872c3387d6de3446500c45a5a2d5ac
Xegendvy <vystudent68@gmail.com>
2024-03-31 14:03:19 +0530

    added multiple functions

--- firstFunction.java
+++ firstFunction.java
@@ -3,6 +3,24 @@ public class firstFunction {
     public static int addNumber(int a, int b){
         return a+b;
     }
+    public static int multipyNumber(int a, int b){
+        return a*b;
+    }
+    public static int subtractNumber(int a, int b){
+        return a-b;
+    }
+    public static int divideNumber(int a, int b){
+        return a/b;
+    }
+    public static int modNumber(int a, int b){
+        return a%b;
+    }
+    public static int factorial(int a){
+        if (a==0){
+            return 1;
+        }
+        return a*factorial(a-1);
+    }
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
         System.out.println("What's a? ");
@@ -11,6 +29,7 @@ public class firstFunction {
         int b = scan.nextInt();
         scan.close();
         int c=addNumber(a,b);
+        System.out.println(factorial(a));
         System.out.println("The sum of "+a+" + "+b+" is "+c);
     }
 }
