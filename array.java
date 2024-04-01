23621a1387ddb0c82027ab1b6b26ea4d951cda9c
Xegendvy <vystudent68@gmail.com>
2024-04-01 10:39:13 +0530

    added arrays in array.java

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        int[] newArr = new int[10];
+        // int newArr[] = new int[10] This is also a valid syntax.
+        newArr[0]=12;
+        for(int i=0;i<10;i++){
+            System.err.println(newArr[i]);
+        }
+    }
+}
